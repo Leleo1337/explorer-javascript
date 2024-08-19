04c3e8c1bb504774bad6a9f6a465f931d2690984
Leleo1337 <168025120+Leleo1337@users.noreply.github.com>
2024-08-19 18:18:26 -0300

    arrumei as pastas

---
+++ aulas/0conceitos/3funcao/ex017.js
@@ -0,0 +1,13 @@
+const sum = function(n1,n2){
+    let total = n1 + n2
+    return total
+}
+
+// usar n1 aqui e n2 funciona igual
+let numero1 = 25
+let numero2 = 20
+
+
+console.log(`o numero 1 é ${numero1}`)
+console.log(`o numero 2 é ${numero2}`)
+console.log(`a soma desses dois numeros é ${sum(numero1, numero2)}`)
