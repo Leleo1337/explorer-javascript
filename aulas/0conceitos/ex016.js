acf40bcc651fcad61093bff805243ea306937d64
Leleo1337 <168025120+Leleo1337@users.noreply.github.com>
2024-08-19 16:53:15 -0300

    ex 16

---
+++ aulas/0conceitos/ex016.js
@@ -0,0 +1,10 @@
+//function expression - anonymous function
+// parametros da função = n1 , n2
+const sum = function (n1,n2){
+    console.log ( n1 + n2)    
+}
+
+// n1 recebe 2 , n2 recebe 3 / separado pela virgula, qual variavel ganha quem
+sum(2,3) // arguments
+sum(10,5)
+sum(500,500)
